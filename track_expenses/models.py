0941dda3ce1967691ab0cc436c72eafd4d2b97df
unknown <m4manishkrswami@gmail.com>
2025-04-06 14:01:41 +0530

    first commit

---
+++ track_expenses/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+import uuid
+from django.contrib.auth.models import User
+
+class BaseModel(models.Model):
+    uuid = models.UUIDField(default=uuid.uuid4,primary_key=True, editable=False, unique=True)
+    created_at = models.DateField(auto_now=True)
+    updated_at = models.DateField(auto_now_add=True)
+
+    class Meta:
+        abstract = True
+
+class Transactions(BaseModel):
+    description = models.CharField(max_length=100)
+    amount = models.FloatField()
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+
+    class Meta:
+        ordering = ('description',)
+
+        
+    def isNegative(self):
+        return self.amount < 0
